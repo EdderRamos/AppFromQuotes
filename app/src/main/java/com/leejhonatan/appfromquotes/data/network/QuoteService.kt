197bc2dfc04cd4706a11740409bfdc2322012dfc
Jhonatan <lee_jhonatan_22@hotmail.com>
2022-11-18 16:55:33 -0500

    "He arreglado el error de Quote Servise"

--- app/src/main/java/com/leejhonatan/appfromquotes/data/network/QuoteService.kt
+++ app/src/main/java/com/leejhonatan/appfromquotes/data/network/QuoteService.kt
@@ -1,12 +1,16 @@
 package com.leejhonatan.appfromquotes.data.network
 
 import com.leejhonatan.appfromquotes.data.model.QuoteModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class QuoteService @Inject constructor(private val api: QuoteApiClient) {
 
     suspend fun getAllQuote(): List<QuoteModel> {
-        val response = api.getQuote()
-        return response.body() ?: emptyList()
+        return withContext(Dispatchers.IO) {
+            val response = api.getQuote()
+            response.body() ?: emptyList()
+        }
     }
 }
